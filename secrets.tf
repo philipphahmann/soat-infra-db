b9e56d5eb1f314fadcd5542979cb2e8da6f11299
Philipp Hahmann <hahmann96@gmail.com>
2025-09-08 22:34:51 -0300

    fix: update AWS Secrets Manager secret name to a static value

--- secrets.tf
+++ secrets.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_password_secret" {
-  name = "${var.project_name}-db-password-secret"
+  name = "db-password-secret"
 }
 
 resource "aws_secretsmanager_secret_version" "db_password_secret_version" {
